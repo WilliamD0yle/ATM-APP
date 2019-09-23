d6bf07ad4657437c0141ead353e13579192ee815
William Doyle <wdoyle@equiis.com>
2019-09-23 20:57:46 +0100

    Updates

---
+++ precache-manifest.70c4aaf6f020e4c423fe68decdc18bba.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7575d8e40a426f43c33d651e1fbe781b",
+    "url": "/ATM-APP/index.html"
+  },
+  {
+    "revision": "65fbfbaae76d30799e62",
+    "url": "/ATM-APP/static/css/main.e25dfe47.chunk.css"
+  },
+  {
+    "revision": "e43529b4682f729af83a",
+    "url": "/ATM-APP/static/js/2.01a6428e.chunk.js"
+  },
+  {
+    "revision": "65fbfbaae76d30799e62",
+    "url": "/ATM-APP/static/js/main.a111a347.chunk.js"
+  },
+  {
+    "revision": "fafd04bf271f72473018",
+    "url": "/ATM-APP/static/js/runtime-main.3e9464c6.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/ATM-APP/static/media/logo.25bf045c.svg"
+  }
+]);
